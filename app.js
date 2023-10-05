4d5bad0f38ab5a90f5b97bcca190c9ca5ebdefa3
developer-metalhead <satwikshah2000@gmail.com>
2023-10-05 23:21:08 +0530

    before editing databse

--- app.js
+++ app.js
@@ -1,35 +1,56 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
-const request = require("request");
-const https = require("https");
-
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 const dotenv = require("dotenv");
 dotenv.config();
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
 
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const app = express();
 const homeStartingContent = "The blogs will be shown here on this Home page.";
 const aboutContent =
   "Uncover, Inspire, and Thrive: Where Curiosity Meets Knowledge ";
 
 let posts = [];
 
-
-
-const app = express();
-
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(cookieParser());
+app.enable("trust proxy");
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+app.use(
+  session({
+    secret: "Ourlittlesecret.",
+    resave: true,
+    saveUninitialized: true,
+    proxy: true,
+    store: MongoStore.create({
+      mongoUrl:
+        "mongodb+srv://admin-satwik:satwik@cluster0.bzlemkr.mongodb.net/test?retryWrites=true&w=majority",
+    }),
+    cookie: {
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 const postSchema = {
   title: String,
@@ -37,8 +58,57 @@ const postSchema = {
 };
 
 const Post = mongoose.model("Post", postSchema);
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  googleId: String,
+  secret: [String],
+});
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    cb(null, { id: user.id, username: user.username, name: user.displayName });
+  });
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.CLIENT_ID,
+//       clientSecret: process.env.CLIENT_SECRET,
+//       callbackURL: "https://gray-gentle-tick.cyclic.app/auth/google/secrets",
+//       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+//     },
+//     function (accessToken, refreshToken, profile, cb) {
+//       console.log(profile);
+//       User.findOrCreate(
+//         { username: profile.displayName, googleId: profile.id },
+//         function (err, user) {
+//           return cb(err, user);
+//         }
+//       );
+//     }
+//   )
+// );
 
 app.get("/", function (req, res) {
+  res.render("splash");
+});
+
+
+app.get("/blog", function (req, res) {
   Post.find({}, function (err, posts) {
     res.render("home", {
       startingContent: homeStartingContent,
@@ -46,6 +116,123 @@ app.get("/", function (req, res) {
     });
   });
 });
+
+
+// app.get(
+//   "/auth/google",
+//   passport.authenticate("google", { scope: ["profile"] })
+// );
+
+// app.get(
+//   "/auth/google/secrets",
+//   passport.authenticate("google", { failureRedirect: "/login" }),
+//   function (req, res) {
+//     // Successful authentication, redirect to secrets.
+//     res.redirect("/secrets");
+//   }
+// );
+
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/blog", function (req, res) {
+  if (req.isAuthenticated()) {
+    User.find({ secret: { $ne: null } })
+      .then((foundUsers) => {
+        if (foundUsers) {
+          res.render("home", { usersWithSecrets: foundUsers });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+
+
+app.post("/register", function (req, res) {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/blog");
+        });
+      }
+    }
+  );
+});
+
+app.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/");
+  });
+});
+
+app.post("/login", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/blog");
+      });
+    }
+  });
+});
+
+app.post("/submit", function (req, res) {
+  const submittedSecret = req.body.secret;
+
+  User.findById(req.user.id)
+    .then((foundUser) => {
+      if (foundUser) {
+        foundUser.secret.push(submittedSecret);
+        foundUser.save().then(() => {
+          res.redirect("/blog");
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.listen(process.env.PORT || 7000, function () {
+  console.log("Server started successfully");
+});
+
+
+
+
+
+
+
+
+
+
+
 
 app.get("/compose", function (req, res) {
   res.render("compose");
@@ -59,7 +246,7 @@ app.post("/compose", function (req, res) {
 
   post.save(function (err) {
     if (!err) {
-      res.redirect("/");
+      res.redirect("/blog");
     }
   });
 });
@@ -114,9 +301,7 @@ app.post("/", function (req, res) {
     if (response.statusCode === 200) {
       res.render("success");
     } else res.render("failure");
-    response.on("data", function (data) {
-
-    });
+    response.on("data", function (data) {});
   });
   request.write(jsonData);
   request.end();
